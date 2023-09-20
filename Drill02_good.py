f83c4e9bde02e4590b9de506215c4b5463385608
dlarlwns135 <dlarlwns135@naver.com>
2023-09-20 18:08:42 +0900

    run_circle완성 run_ractengle 조금 완성
    ㅇ

--- Drill02_good.py
+++ Drill02_good.py
@@ -9,10 +9,25 @@ character = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
-    pass
+
+    cx, cy, r = 400, 300, 200
+    for deg in range(0, 360, 5):
+        x = cx + r * math.cos(math.radians(deg))
+        y = cy + r * math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.1)
+
 
 def run_rectangle():
     print('CURCLE')
+
+    for x in range(50, 750+1, 10):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        delay(0.1)
     pass
 
 while True:
