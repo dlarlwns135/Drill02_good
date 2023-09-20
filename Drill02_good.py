bfc6c97ec9332e19eb09fcf813b2e12c582476e5
dlarlwns135 <dlarlwns135@naver.com>
2023-09-20 17:44:02 +0900

    테스트 성공
    ㅁ

--- Drill02_good.py
+++ Drill02_good.py
@@ -1,5 +1,23 @@
-from pico2d imprt *
+import math
+
+from pico2d import *
 
 open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('CURCLE')
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+    #break;
 
 close_canvas()
