5edefe494fab8d06689971df2faccd9f1b157fd3
dlarlwns135 <dlarlwns135@naver.com>
2023-09-20 18:12:27 +0900

    render_all 함수 생성
    바뀐 좌표값에 대한 그림그리는 함수

--- Drill02_good.py
+++ Drill02_good.py
@@ -7,6 +7,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def render_all(x,y):
+     clear_canvas_now()
+     grass.draw_now(400,30)
+     character.draw_now(x,y)
+     delay(0.01)
+
 def run_circle():
     print('CIRCLE')
 
@@ -14,20 +20,14 @@ def run_circle():
     for deg in range(0, 360, 5):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)
+        render_all(x,y)
 
 
 def run_rectangle():
     print('CURCLE')
 
     for x in range(50, 750+1, 10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)
+       render_all(x,90)
     pass
 
 while True:
